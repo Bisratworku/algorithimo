dc39e0af354c469946e280b980c00502f22aa99f
Bisratworku <101055036+Bisratworku@users.noreply.github.com>
2024-05-01 12:51:27 -0700

    simplefied the code

--- Algorithimo/sorting/Divide_and_conquer.cpp
+++ Algorithimo/sorting/Divide_and_conquer.cpp
@@ -2,35 +2,35 @@
 #include <iostream>
 using namespace std;
 
-int Find_the_max_crossing_subarray(int arr[], int low, int mid, int high){
-    int  left_sum = -100000000;
-    int left_max = 0;
-    int l_sum = 0;
-    for(int i = mid; i >= low; i--){
-        l_sum += arr[i];
-        if(l_sum > left_sum){
-            left_sum = l_sum;
-            left_max = i;
+int Find_Max_Subarray(int a[], int high, int low){
+    if(high == low){
+        return low, high;
+    }else{
+        int mid = (high + low)/2;
+        int max_l = 0;
+        for(int i = mid; i >= 0; i--){
+            max_l += a[i];
+        }
+        int max_r = 0;
+        for(int j = mid + 1 ; j <= high; j++){
+            max_r += a[j];
+        }
+        if(max_r > max_l){
+            return Find_Max_Subarray(a, high , low + mid );
+        }
+        else{
+            return Find_Max_Subarray(a, high - mid, low);
         }
     }
-    int right_sum = -10000000;
-    int right_max = 0;
-    int r_sum = 0;
-    for(int i = mid + 1; i <= high; i++){
-        r_sum += arr[i];
-        if(r_sum > right_sum){
-            right_sum = r_sum;
-            right_max = i;
-        }
-
-    }
-    return (left_max, right_max, left_sum + right_sum);
+    return 0;
 }
 
-int main(){
-    int arr[10] = {2,4,6,8,10,1,9,7,3,5};
-    Find_the_max_crossing_subarray(arr, 0, 5, 10);
-    
+
+int main(){    
+    int arr[5] = {1,2,3,4,5};
+    int low, high = Find_Max_Subarray(arr, 4, 0);
+    //cout<<low<<endl;
+    //cout<<high<<endl;
     return 0;
 
 }
